0893bc5c9950ce1f4c597fc97393d51354660414
Sabriye Nur <79248086+sabisay@users.noreply.github.com>
2024-11-16 23:32:03 +0300

    src and html files delivered

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="main_title_container">
+      <h1 id='main_title'>Merhaba Meraklı Okur!</h1>
+    </div>
+  );
+}
+
+export default App;
